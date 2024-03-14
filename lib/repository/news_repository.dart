7f89a1570cf2b1346434a598dbc11f83cfd49f40
praty474 <p.kayastha.pk@gmail.com>
2024-03-14 21:29:07 +0545

    categories_added

--- lib/repository/news_repository.dart
+++ lib/repository/news_repository.dart
@@ -21,7 +21,7 @@ class NewsRepository {
 
   Future<CategoryNewsModel> fetchCategoriesNewsApi(String category) async {
     String url =
-        'https://newsapi.org/v2/everything?q=${category}&apiKey=5c5afad730af41eca231d3cf266ec413';
+        'https://newsapi.org/v2/everything?q=general&apiKey=5c5afad730af41eca231d3cf266ec413';
 
     final response = await http.get(Uri.parse(url));
 
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:quiz/models/category_news_model.dart';
+import 'package:quiz/screens/home_screen.dart';
+import 'package:quiz/view_model/news_view_model.dart';
 
 class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key});
@@ -9,10 +16,129 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
+  NewsViewModel newsViewModel = NewsViewModel();
+
+  FilterList? selectedMenu;
+
+  final format = DateFormat('MMMM dd, yyyy');
+  String categoryName = 'general';
+
+  List<String> categoriesList = [
+    'General',
+    'Entertainment',
+    'Health',
+    'Sports',
+    'Technology',
+    'Business'
+  ];
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height * 1;
+    final width = MediaQuery.sizeOf(context).width * 1;
     return Scaffold(
       appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categoriesList.length,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      splashColor: Colors.white,
+                      onTap: () {
+                        categoryName = categoriesList[index];
+                        setState(() {});
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 12),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(15),
+                            color: categoryName == categoriesList[index]
+                                ? Colors.blue
+                                : Colors.grey,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12),
+                            child: Center(
+                                child: Text(
+                              categoriesList[index].toString(),
+                              style: GoogleFonts.poppins(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700),
+                            )),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            Expanded(
+              child: FutureBuilder<CategoryNewsModel>(
+                future: newsViewModel.fetchCategoriesNewsApi(categoryName),
+                builder: (BuildContext context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: SpinKitCircle(
+                        size: 40,
+                        color: Colors.amber,
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text('Error: ${snapshot.error}'),
+                    );
+                  } else if (snapshot.data == null ||
+                      snapshot.data!.articles == null) {
+                    return Center(
+                      child: Text('No data available'),
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.articles!.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        DateTime dateTime = DateTime.parse(snapshot
+                            .data!.articles![index].publishedAt
+                            .toString());
+                        return Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image.asset(
+                                'assets/images/flower.png',
+                                height: height * .2,
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                height: height * .18,
+                                padding: EdgeInsets.only(left: 15),
+                                child: Column(
+                                  children: [
+                                    Text(snapshot.data!.articles![index].title
+                                        .toString())
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
